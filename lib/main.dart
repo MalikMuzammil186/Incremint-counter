38d73064a20e159c908bc8d5517973f8d73a5697
MalikMuzammil186 <shoukatmohammad97@gmail.com>
2024-03-14 12:46:00 +0500

    title change

--- lib/main.dart
+++ lib/main.dart
@@ -16,7 +16,7 @@ class MyApp extends StatelessWidget {
     return ProviderScope(
       child: MaterialApp(
         debugShowCheckedModeBanner: true,
-        title: 'Food items',
+        title: 'Food Item Increment counter',
         theme: ThemeData(
           colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
           useMaterial3: true,
